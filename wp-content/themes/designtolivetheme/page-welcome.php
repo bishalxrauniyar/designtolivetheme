c36fe134ba73de8feab609546983c3a41369ea9e
bishalxrauniyar <Bishal.rauniyar@gmail.com>
2025-01-29 16:21:35 +0545

    template hierarchy

---
+++ wp-content/themes/designtolivetheme/page-welcome.php
@@ -0,0 +1,22 @@
+<?php
+get_header();
+die('sfsdfsdfds');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Welcome Page</h1>
+</body>
+
+</html>
+
+<?php
+get_footer();
+?>
